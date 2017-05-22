dd8327780e34114824429a3f471acd9e9b9f131f
Geo Pertea <geo.pertea@gmail.com>
2017-05-22 17:29:12 -0400

    version bumped to 0.10.1

--- gffcompare.cpp
+++ gffcompare.cpp
@@ -3,7 +3,7 @@
 #include <errno.h>
 #include "gtf_tracking.h"
 
-#define VERSION "0.9.9e"
+#define VERSION "0.10.1"
 
 #define USAGE "Usage:\n\
 gffcompare [-r <reference_mrna.gtf> [-R]] [-T] [-V] [-s <seq_path>]\n\
