c795a609459827db946878cb1ab5966dd15269cb
Geo Pertea <geo.pertea@gmail.com>
2021-07-29 17:49:16 -0400

    prep_source fix

--- prep_source.sh
+++ prep_source.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 ver=$(fgrep '#define VERSION ' gffcompare.cpp)
 ver=${ver#*\"}
 ver=${ver%%\"*}
@@ -13,7 +13,7 @@ libdir=$pack/gclib/
 cp LICENSE README.md gffcompare.cpp gtf_tracking.{h,cpp} \
  trmap.cpp $pack/
 sed 's|\.\./gclib|./gclib|' Makefile > $pack/Makefile
-cp ../gclib/{GVec,GList,GAIList,GHashMap,khashl}.hh ../gclib/wyhash.h ../gclib/xxhash.h ../gclib/GBitVec.h $libdir
+cp ../gclib/{GVec,GList,GAIList,GHashMap,GRadixSorter,khashl}.hh ../gclib/wyhash.h ../gclib/xxhash.h ../gclib/GBitVec.h $libdir
 cp ../gclib/{GArgs,GBase,gdna,gff,codons,GFaSeqGet,GFastaIndex}.{h,cpp} $libdir
 tar cvfz $pack.tar.gz $pack
 ls -l $pack.tar.gz
